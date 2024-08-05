aa416cc9a16e997f204e382a49bd75d933cf26b2
Zexa <zexa@cock.li>
2024-08-05 16:11:23 +0300

    Remove old docs

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -2,35 +2,6 @@ use core::fmt::Display;
 use scraper::ElementRef;
 use std::error::Error;
 
-/// de_hypertext is a rust framework for deserializing html into structs.
-///
-/// ```rust
-/// #[derive(Debug, de_hypertext::Deserialize)]
-/// struct MyStruct {
-///     #[de_hypertext(".title")]
-///     title: String,
-/// }
-///
-/// fn main() {
-///     let html = r#"<div><h1 class="title">Example Domain</h1></div>"#;
-///     let result = MyStruct::from_html(html).unwrap()
-///     println!("{result:#?}");
-/// }
-/// ```
-///
-/// If an element is not found, Error will be returned. This functionality can
-/// be overwritten by using Option<T>, which will return Some(T) when
-/// the element is found or None if it is not.
-///
-/// usize may be used with `#[de_hypertext(count)]` to return the amount of
-/// times an element was found.
-///
-/// The first match is used. Except when requesting Vec<T>.
-///
-///
-/// `de-hypertext-finder` is an app that helps you find updated selectors
-/// for structs that derive `de_hypertext::Deserialize`.
-///
 pub trait Deserializer<T> {
     fn from_document(document: &ElementRef) -> Result<T, Box<dyn Error>>;
 
