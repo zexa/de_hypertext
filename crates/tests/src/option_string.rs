1165724eb2a7f7422a2c3602449dd72dfd3c394f
Zexa <zexa@cock.li>
2024-09-03 20:52:10 +0300

    chore: add test_option_string_attribute_impl

--- crates/tests/src/option_string.rs
+++ crates/tests/src/option_string.rs
@@ -37,39 +37,39 @@ fn test_option_string_impl() {
     crate::assert_tokens_eq(expected, actual);
 }
 
-// #[test]
-// fn test_option_string_attribute_impl() {
-//     let input: DeriveInput = parse_quote! {
-//         struct OptionStringSelector {
-//             #[de_hypertext(selector = "a", attribute = "href")]
-//             field1: Option<String>,
-//         }
-//     };
-//     let expected: TokenStream = quote! {
-//         impl de_hypertext::Deserializer<Self> for HasOption {
-//             fn from_document(
-//                 document: &de_hypertext::scraper::ElementRef,
-//             ) -> Result<Self, de_hypertext::DeserializeError> {
-//                 let field1 = {
-//                     let selector = de_hypertext::scraper::Selector::parse("a").map_err(|_| {
-//                         de_hypertext::DeserializeError::BuildingSelectorFailed {
-//                             struct_name: std::any::type_name::<Self>().to_string(),
-//                             field: "field1".to_string(),
-//                             selector: "a".to_string(),
-//                         }
-//                     })?;
-//                     match document.select(&selector).next() {
-//                         Some(document) => document
-//                             .value()
-//                             .attr("href")
-//                             .map(|attribute| attribute.trim().to_string()),
-//                         None => None,
-//                     }
-//                 };
-//                 Ok(Self { field1 })
-//             }
-//         }
-//     };
-//     let actual = impl_derive_deserialize(input);
-//     crate::assert_tokens_eq(expected, actual);
-// }
+#[test]
+fn test_option_string_attribute_impl() {
+    let input: DeriveInput = parse_quote! {
+        struct OptionStringSelectorAttribute {
+            #[de_hypertext(selector = "a", attribute = "href")]
+            field1: Option<String>,
+        }
+    };
+    let expected: TokenStream = quote! {
+        impl de_hypertext::Deserializer<Self> for OptionStringSelectorAttribute {
+            fn from_document(
+                document: &de_hypertext::scraper::ElementRef,
+            ) -> Result<Self, de_hypertext::DeserializeError> {
+                let field1 = {
+                    let selector = de_hypertext::scraper::Selector::parse("a").map_err(|_| {
+                        de_hypertext::DeserializeError::BuildingSelectorFailed {
+                            struct_name: std::any::type_name::<Self>().to_string(),
+                            field: "field1".to_string(),
+                            selector: "a".to_string(),
+                        }
+                    })?;
+                    match document.select(&selector).next() {
+                        Some(document) => document
+                            .value()
+                            .attr("href")
+                            .map(|attribute| attribute.trim().to_string()),
+                        None => None,
+                    }
+                };
+                Ok(Self { field1 })
+            }
+        }
+    };
+    let actual = impl_derive_deserialize(input);
+    crate::assert_tokens_eq(expected, actual);
+}
